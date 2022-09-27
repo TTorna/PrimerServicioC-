fed032073fe02cb3045902661a52d56a876c4ef9
Tomas Tornamira <103047119+TTorna@users.noreply.github.com>
2022-09-27 08:28:30 -0300

    Add files via upload

---
+++ PrimerServicio en cCHAR/PrimerServicio en cCHAR/Service1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace PrimerServicio_en_cCHAR
+{
+    using System.IO;
+
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+        public System.Timers.Timer TimerServicio = new System.Timers.Timer();
+
+        protected override void OnStart(string[] args)
+        {
+            // Agregue el código aquí para iniciar el servicio. Este método debería poner
+            // en movimiento los elementos para que el servicio pueda funcionar.        
+            TimerServicio = new System.Timers.Timer();
+            TimerServicio.Elapsed += ( _,__ )=>EjecutaUnaAccion();
+            TimerServicio.Interval = 6000;
+            TimerServicio.Start();
+        }
+
+        public void EjecutaUnaAccion()
+        {
+            int i;
+            for (i = 1; i <= 1000; i++)
+                File.WriteAllText(@"E:\INFORME.TXT", "LINEA: " + i + System.Environment.NewLine);
+        }
+
+        protected override void OnStop()
+        {
+            // Agregue el código aquí para realizar cualquier anulación necesaria para detener el servicio.
+            TimerServicio.Close();
+        }
+        protected override void OnPause()
+        {
+            TimerServicio.Stop();
+        }
+
+        protected override void OnContinue()
+        {
+            TimerServicio.Start();
+        }
+    }
+}
